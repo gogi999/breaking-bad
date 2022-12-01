8d95517cc1132022f4709f922d7005be5b8bde6a
gogi999 <g.petrovic9@gmail.com>
2022-12-01 13:07:54 +0100

    Breaking Bad App Completed

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+
+import axios from 'axios';
+
+import CharacterGrid from './components/characters/CharacterGrid';
+import Header from './components/ui/Header';
+import Search from './components/ui/Search';
+
+const App = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+            setItems(result.data);
+            setIsLoading(false);
+        }
+
+        fetchItems();
+    }, [query]);
+
+    return (
+        <div className="container">
+            <Header />
+            <Search getQuery={(q) => setQuery(q)} />
+            <CharacterGrid isLoading={isLoading} items={items} />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/characters/CharacterGrid.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Spinner from '../ui/Spinner';
+import CharacterItem from './CharacterItem';
+
+const CharacterGrid = ({ isLoading, items }) => {
+    return isLoading ? (
+            <Spinner />
+        ) : (
+            <section className="cards">
+                {items?.map((item) => (
+                    <CharacterItem key={item.char_id} item={item} />
+                ))}
+            </section>
+        )
+}
+
+export default CharacterGrid;
